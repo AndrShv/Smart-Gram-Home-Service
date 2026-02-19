70015de6a3f41a1ed9fb31dbaa5637a5eba30b3f
andrshv <andrey678a@gmail.com>
2026-02-19 16:50:05 +0200

    Сделал так что б погружаалсь все инфа о профиле в home сервисе в вкладке profile, скоро буду тестировать и тд

--- src/main/java/com/example/project/HomeApplication.java
+++ src/main/java/com/example/project/HomeApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.example.project.clients")
 public class HomeApplication {
         public static void main(String[] args) {
             SpringApplication.run(HomeApplication.class, args);
-
             //docker exec -it homeservice-mysql mysql -u root -p
 
         }
